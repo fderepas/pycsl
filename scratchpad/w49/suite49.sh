79f0e8e9e13c4edc8a2bedbbcce1f3af87f60ca1
fabrice <fabrice.derepas@polytechnique.org>
2026-09-09 08:43:25 +0000

    m

--- scratchpad/w49/suite49.sh
+++ scratchpad/w49/suite49.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/fabrice/git/pycsl
 export PATH=/home/fabrice/.opam/framac-coq8/bin:$PATH
-bash bin/run-reference-tests.sh --jobs 3 > getting-better/proofs49/suite49_run1.log 2>&1
-echo $? > getting-better/proofs49/suite49_run1.rc
-echo "$(date -u +%H:%M) SUITE run1 rc=$(cat getting-better/proofs49/suite49_run1.rc)" >> getting-better/proofs49/queue49.progress
+bash bin/run-reference-tests.sh --jobs 3 > getting-better/proofs49/suite49_run2.log 2>&1
+echo $? > getting-better/proofs49/suite49_run2.rc
+echo "$(date -u +%H:%M) SUITE run2 rc=$(cat getting-better/proofs49/suite49_run2.rc)" >> getting-better/proofs49/queue49.progress
